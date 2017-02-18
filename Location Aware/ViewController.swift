22904e67df84a7b1de1dc97b932360254c709dcd
vks4303 <vks4303@gmail.com>
2017-02-19 00:53:32 +0530

    Initial Commit

---
+++ Location Aware/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Location Aware
+//
+//  Created by Vaibhav Kumar Singh on 19/02/17.
+//  Copyright © 2017 Vaibhav Kumar Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
